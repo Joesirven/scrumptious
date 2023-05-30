78cd70475f91529bb4bc1c2df23215d5c6f595c3
Jose <jose@oldcutler.club>
2023-05-30 19:32:50 -0400

    Set up model, admim, create super user

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from recipes.models import Recipe
 
 # Register your models here.
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "id",
+    )
